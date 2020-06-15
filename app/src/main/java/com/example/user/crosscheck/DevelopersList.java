f7e1241fac5aeb26c6afdffff1f55cb53fea4640
oluwadamola <stephenajoses@gmail.com>
2020-06-15 08:40:15 +0100

    initial commit

---
+++ app/src/main/java/com/example/user/crosscheck/DevelopersList.java
@@ -0,0 +1,30 @@
+package com.example.user.crosscheck;
+
+/**
+ * Created by jude on 2017-10-21.
+ */
+
+public class DevelopersList {
+
+    private String login;
+    private String avatar_url;
+    private String html_url;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public DevelopersList(String login, String gitUrl, String avatar_url) {
+        this.login = login;
+        this.avatar_url = avatar_url;
+        this.html_url = gitUrl;
+    }
+}
